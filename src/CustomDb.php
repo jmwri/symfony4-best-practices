8e71d4c6913f5b345b1c8976a82550c064a0490e
Jim Wright <jim@linx.net>
2017-09-22 16:02:13 +0100

    Automatically inject constructor params for a service

--- src/CustomDb.php
+++ src/CustomDb.php
@@ -4,6 +4,15 @@ namespace App;
 
 class CustomDb
 {
+    private $host;
+    private $port;
+
+    public function __construct($host, $port)
+    {
+        $this->host = $host;
+        $this->port = $port;
+    }
+
     public function getUsers()
     {
         return [
